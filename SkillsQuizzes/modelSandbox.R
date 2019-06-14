374049ab0f2068e6e328dde9e35de803105d2a45
Dallin <dallinis@hotmail.com>
2019-06-14 05:40:23 -0600

    infinity cubics

--- SkillsQuizzes/modelSandbox.R
+++ SkillsQuizzes/modelSandbox.R
@@ -1,70 +1,70 @@
 set.seed(151)
 
-n <- 3000
+n <- 80
 
 X5 <- runif(n, -10, 10)
-X2 <- sample(c('blue pill', 'red pill', 'mystery pill', 'green pill'), n, replace=TRUE)
+X2 <- sample(c('blue pill', 'red pill'), n, replace=TRUE)
 
-blue <- ifelse(X2 == 'blue pill', 1, 0)
+X3 <- sample(c('one fish', 'two fish', 'red fish', 'blue fish'), n, replace=TRUE)
+
+X6 <- sample(c('steak', 'chicken', 'mystery meat'), n, replace=TRUE)
+
+X8 <- runif(n, -10, 10)
+
 red <- ifelse(X2 == 'red pill', 1, 0)
-green <- ifelse(X2 == 'green pill', 1, 0)
-mystery <- ifelse(X2 == 'mystery pill', 1, 0)
 
-beta0 <- 4
-beta1 <- .2
+beta0 <- -5
+beta1 <- .6
 beta2 <- .03
 beta3 <- -.01
 
-beta4 <- 1
-beta5 <- -.5
-beta6 <- .02
+beta4 <- 3
+beta5 <- -1.2
+beta6 <- -.06
 beta7 <- .02
 
-beta8 <- 5
+beta8 <- -10
 beta9 <- -.09
-beta10 <- -.18
-beta11 <- .009
+beta10 <- 0
+beta11 <- 0
 
-beta12 <- -2
-beta13 <- -.9
+beta12 <- -10
+beta13 <- .2
 beta14 <- -.02
 beta15 <- 0
 
-beta16 <- -10
-beta17 <- -.1
+beta16 <- 8
+beta17 <- -.01
 beta18 <- .1
-beta19 <- 0
+beta19 <- .01
 
 sigma <- 5
 
 Y <- 
      beta0 + beta1*X5 + beta2*X5^2 + beta3*X5^3 +
-     beta4*blue + beta5*X5*blue + beta6*X5^2*blue + beta7*X5^3*blue +
-     beta8*red + beta9*X5*red + beta10*X5^2*red + beta11*X5^3*red +
-     beta12*green + beta13*X5*green + beta14*X5^2*green + beta15*X5^3*green +
-     beta16*mystery + beta17*X5*mystery + beta18*X5^2*mystery + beta19*X5^3*mystery +
+     beta4*red + beta5*X5*red + beta6*X5^2*red + beta7*X5^3*red +
   rnorm(n, 0, sigma)
 
 myData <- data.frame(
             Y = Y
             ,
-            X1 = rf(n, 3, 9)
+            X1 = rbeta(n,5,2)
             ,
             X2 = X2
             ,
-            X3 = sample(c(0,1), n, replace=TRUE)
+            X3 = X3
             ,
             X4 = rf(n, 2, 5)
             ,
             X5 = X5
             ,
-            X6 = rf(n, 2, 5) + rt(n, 1)^2
+            X6 = X6
             ,
             X7 = sample(c(0,1), n, replace=TRUE)
             ,
             X8 = runif(n, -20, 5)
             ,
-            X9 = rpois(n, 3.2)
+            X9 = rpois(n, 1)
             ,
             X10 = rbinom(n, 30, .5)
           )
@@ -72,7 +72,7 @@ myData <- data.frame(
 plot(Y ~ X5, data = myData)
 
 
-# pairs(myData)
+pairs(myData)
 
 mylm <- lm(Y ~ X5 + X2 + X5:X2 + I(X5^2) + I(X5^3) + I(X5^2):X2 + I(X5^3):X2, data = myData)
 
@@ -80,11 +80,8 @@ summary(mylm)
 
 b <- coef(mylm)
 
-curve(b[1] + b[2]*x + b[6]*x^2 + b[7]*x^3, add=TRUE, col=palette()[1], lwd=4)
-curve((b[1] + b[3]) + (b[2] + b[8])*x + (b[6] + b[11])*x^2 + (b[7] + b[14])*x^3, add=TRUE, col=palette()[1], lwd=4)
-curve((b[1] + b[4]) + (b[2] + b[9])*x + (b[6] + b[12])*x^2 + (b[7] + b[15])*x^3, add=TRUE, col=palette()[1], lwd=4)
-curve((b[1] + b[4]) + (b[2] + b[9])*x + (b[6] + b[12])*x^2 + (b[7] + b[15])*x^3, add=TRUE, col=palette()[1], lwd=4)
-curve((b[1] + b[5]) + (b[2] + b[10])*x + (b[6] + b[13])*x^2 + (b[7] + b[16])*x^3, add=TRUE, col=palette()[1], lwd=4)
+curve(b[1] + b[2]*x + b[4]*x^2 + b[5]*x^3, add=TRUE, col=palette()[1], lwd=4)
+curve((b[1] + b[3]) + (b[2] + b[6])*x + (b[4] + b[7])*x^2 + (b[5] + b[8])*x^3, add=TRUE, col=palette()[1], lwd=4)
 
 # lm1 <- lm(Y ~ X4, data = myData)
 # 
