e3d03b41cce56b6fbc25b1956ca3d00a90995cd3
Dallin <dallinis@hotmail.com>
2019-06-14 10:28:57 -0600

    two cubics two lines

--- SkillsQuizzes/modelSandbox.R
+++ SkillsQuizzes/modelSandbox.R
@@ -4,6 +4,7 @@ n <- 80
 
 X5 <- runif(n, -10, 10)
 X2 <- sample(c('blue pill', 'red pill'), n, replace=TRUE)
+X9 <- sample(c(0, 1), n, replace=TRUE)
 
 X3 <- sample(c('one fish', 'two fish', 'red fish', 'blue fish'), n, replace=TRUE)
 
@@ -23,26 +24,23 @@ beta5 <- -1.2
 beta6 <- -.06
 beta7 <- .02
 
-beta8 <- -10
-beta9 <- -.09
+beta8 <- 1
+beta9 <- .1
 beta10 <- 0
 beta11 <- 0
 
-beta12 <- -10
-beta13 <- .2
-beta14 <- -.02
+beta12 <- 0
+beta13 <- -.3
+beta14 <- 0
 beta15 <- 0
 
-beta16 <- 8
-beta17 <- -.01
-beta18 <- .1
-beta19 <- .01
-
-sigma <- 5
+sigma <- 2.9
 
 Y <- 
      beta0 + beta1*X5 + beta2*X5^2 + beta3*X5^3 +
      beta4*red + beta5*X5*red + beta6*X5^2*red + beta7*X5^3*red +
+     beta8 + beta9*X5 +
+     beta12*X9 + beta13*X9*X5 +
   rnorm(n, 0, sigma)
 
 myData <- data.frame(
@@ -60,27 +58,29 @@ myData <- data.frame(
             ,
             X6 = X6
             ,
-            X7 = sample(c(0,1), n, replace=TRUE)
+            X7 = sample(c('right','left'), n, replace=TRUE)
             ,
             X8 = runif(n, -20, 5)
             ,
-            X9 = rpois(n, 1)
+            X9 = X9
             ,
             X10 = rbinom(n, 30, .5)
           )
 
 plot(Y ~ X5, data = myData)
 
-pairs(myData)
+# pairs(myData)
 
-mylm <- lm(Y ~ X5 + X2 + X5:X2 + I(X5^2) + I(X5^3) + I(X5^2):X2 + I(X5^3):X2, data = myData)
+mylm <- lm(Y ~ X5 + X2 + X5:X2 + X5:X9 + I(X5^2) + I(X5^3) + I(X5^2):X2 + I(X5^3):X2, data = myData)
 
 summary(mylm)
 
 b <- coef(mylm)
 
-curve(b[1] + b[2]*x + b[4]*x^2 + b[5]*x^3, add=TRUE, col=palette()[1], lwd=4)
-curve((b[1] + b[3]) + (b[2] + b[6])*x + (b[4] + b[7])*x^2 + (b[5] + b[8])*x^3, add=TRUE, col=palette()[1], lwd=4)
+curve(beta0 + beta1*x + beta2*x^2 + beta3*x^3, add=TRUE, col=palette()[1], lwd=4)
+curve((beta0 + beta4) + (beta1 + beta5)*x + (beta2 + beta6)*x^2 + (beta3 + beta7)*x^3, add=TRUE, col=palette()[1], lwd=4)
+abline(beta8, beta9)
+abline((beta8 + beta12), (beta9 + beta13))
 
 # lm1 <- lm(Y ~ X4, data = myData)
 # 
